ed690b67436a5f22def40c4e264ce56af0faafac
Pavvan  Pradeep <pavvanpradeep@Pavvans-MacBook-Air.local>
2022-10-22 02:43:28 +0530

    added the social media section

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,6 +1,8 @@
 import React from "react";
-import img1 from './icons8-github-60.png'
-import img2 from './icons8-phone-50.png'
+import img1 from './icons8-instagram-60.png'
+import img2 from './icons8-facebook-60.png'
+import img3 from './icons8-linkedin-circled-60.png'
+
 
 const Footer = () => (
   <div className="footer">
@@ -8,8 +10,7 @@ const Footer = () => (
             <p>91+9889965738</p>
             <img src={img1} alt="logo insta"></img>
             <img src={img2} alt="logo facebook"></img>
-
-            
+            <img src={img3} alt="logo linkedin"></img>
   </div>
 );
 
