3ce26e6433fa245107735895f4771916200311ef
Pavvan  Pradeep <pavvanpradeep@Pavvans-MacBook-Air.local>
2022-10-21 19:50:35 +0530

    fixed footer

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,18 +1,11 @@
-import React from 'react'
+import React from "react";
 
-export default function ContactList() {
-  return (
-    <>
-     <div className='Contacts'>
-        <section className='ContactList'>
-            <h4>Contact List</h4>
+const Footer = () => (
+  <div className="footer">
+    <h4>Contact List</h4>
             <p>91+9889965738</p>
             <p>91+86789965738</p>
-            <p>91+47559965738</p>
-            <p>91+53399653738</p>
-    </section>
-    </div>
-    </>
-   
-  )
-}
+  </div>
+);
+
+export default Footer;
