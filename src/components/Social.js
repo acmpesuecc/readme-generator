10f3ac4140ffeb2737a67f3036b59ca5d0b84084
Pavvan  Pradeep <pavvanpradeep@Pavvans-MacBook-Air.local>
2022-10-22 02:55:05 +0530

    Updated social media section

--- src/components/Social.js
+++ src/components/Social.js
@@ -6,7 +6,7 @@ import img3 from './icons8-linkedin-circled-60.png'
 export default function Social() {
   return (
     <>
-    <h4>You can reach us on the following</h4>
+    <h4>You can reach us on the following places</h4>
     <p>Click the images to go the site</p>
     <a href="https://www.instagram.com/"><img src={img1} alt="logo insta"></img></a> 
     <a href="https://www.facebook.com/"><img src={img2} alt="logo facebook"></img></a>
